feda4fe77ff912a6e3ab0c9de7fd0e19cc308566
003-demo <rrriku05@gmail.com>
2025-11-14 23:57:22 +0600

    sum

--- problem01.js
+++ problem01.js
@@ -243,8 +243,14 @@
 // }
 // console.log(namearr("RohanKabir"));
 
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+let sum = 0;
 for (let i = 1; i <= 10; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
+  sum = sum + i;
 }
+console.log(sum);
