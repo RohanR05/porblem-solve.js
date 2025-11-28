b60595250ac612b7cd649836af5672a464da2af2
003-demo <rrriku05@gmail.com>
2025-11-28 10:35:54 +0600

    adf

--- problem01.js
+++ problem01.js
@@ -386,3 +386,16 @@
 // }
 
 // console.log(largeNum);
+
+let mainArray = [1, 2, 3, 4, 5, 5, 5, -1, -2];
+let singleArray = [];
+
+for (i = 0; i < mainArray.length; i++) {
+  let num = mainArray[i];
+  if (!singleArray.includes(num)) {
+    singleArray.push(num);
+  }
+}
+singleArray.sort((a, b) => a - b);
+
+console.log(singleArray);
