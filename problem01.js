2f105610d6729349503f2eb6cd1a2d7a2752f21c
003-demo <rrriku05@gmail.com>
2025-12-01 21:57:25 +0600

    asfa

--- problem01.js
+++ problem01.js
@@ -1,11 +1,11 @@
-let mainArray = [1, 2, 3, 4, 5, 5, 4, 9, -1];
-let singleArray = [];
+// let mainArray = [1, 2, 3, 4, 5, 5, 4, 9, -1];
+// let singleArray = [];
 
-for (i = 0; i < mainArray.length; i++) {
-  if (!singleArray.includes(mainArray[i])) {
-    singleArray.push(mainArray[i]);
-  }
-}
-singleArray.sort((a, b) => a - b);
+// for (i = 0; i < mainArray.length; i++) {
+//   if (!singleArray.includes(mainArray[i])) {
+//     singleArray.push(mainArray[i]);
+//   }
+// }
+// singleArray.sort((a, b) => a - b);
 
-console.log(singleArray);
+// console.log(singleArray);
