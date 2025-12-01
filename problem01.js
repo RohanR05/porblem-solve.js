71a0d3b618982c2b1f54d629d98ac8c2c1d2af92
003-demo <rrriku05@gmail.com>
2025-12-01 22:06:53 +0600

    df

--- problem01.js
+++ problem01.js
@@ -1,4 +1,8 @@
-function arr(rev) {
-  return rev.split("").reverse().join("");
+let mainArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let revArray = [];
+
+for (i = 0; i < mainArray.length; i++) {
+  revArray.unshift(mainArray[i]);
 }
-console.log(arr("Rohan Kabir"));
+
+console.log(revArray);
