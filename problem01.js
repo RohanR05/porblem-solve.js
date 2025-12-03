33e4ff08f21c526759dde93df0ab2d275d93e560
003-demo <rrriku05@gmail.com>
2025-12-03 22:02:38 +0600

    adsf

--- problem01.js
+++ problem01.js
@@ -1,4 +1,12 @@
 function totalOfEven(str) {
+  let total = 0;
+  for (let s of str) {
+    if (s % 2 === 0) {
+      total += s;
+    }
+  }
+  console.log(total);
+  return total;
 }
 let str = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 totalOfEven(str);
