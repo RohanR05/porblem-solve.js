9b55fd67acbad87838d23b8f5fc45445ef12d0e6
003-demo <rrriku05@gmail.com>
2025-12-04 23:58:42 +0600

    adf

--- problem01.js
+++ problem01.js
@@ -1,13 +1,13 @@
 function duplicateFunction(str) {
   let singleArray = [];
   for (let s of str) {
-    let num = str[s];
-    if (!singleArray.includes(num)) {
-      singleArray.push(num);
+    if (!singleArray.includes(s)) {
+      singleArray.push(s);
     }
   }
+  singleArray.sort((a, b) => a - b);
   console.log(singleArray);
   return singleArray;
 }
-str = [1, 2, 3, 4, 5, 5, 6, 6, 6, 6, 3, 2, 7, 5];
+str = [1, 2, 3, 4, 5, 5, 6, 6, 6, 6, 3, 2, -7];
 duplicateFunction(str);
