da9f3e5c07bd6b1afb67a007ba8c233234b67e02
003-demo <rrriku05@gmail.com>
2025-12-05 23:21:10 +0600

    adg

---
+++ problem01.js
@@ -0,0 +1,16 @@
+function totalOfOddandEven(str) {
+  let totalOfOdd = [];
+  let totalOfEven = [];
+  for (let s of str) {
+    if (s % 2 === 0) {
+      totalOfEven = s + totalOfEven;
+    } else {
+      totalOfOdd = s + totalOfOdd;
+    }
+  }
+  console.log("Total Of Odd", totalOfOdd);
+  console.log("Total of even", totalOfEven);
+  return { totalOfEven, totalOfOdd };
+}
+str = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+totalOfOddandEven(str);
