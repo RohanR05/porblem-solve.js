2738364990c0e247ba3075df4030afa00a63d4fd
003-demo <rrriku05@gmail.com>
2025-12-08 19:44:09 +0600

    remove duplicate

--- problem01.js
+++ problem01.js
@@ -1,4 +1,4 @@
-let mainArray = [1, 2, 3, 4, 5, 5, 5, 5, 5, 10];
+let mainArray = [1, 2, 3, 4, 5, 5, 5, 5, 5, -10];
 let singleArray = [];
 
 function RemoveDuplicate(str) {
@@ -7,7 +7,7 @@ function RemoveDuplicate(str) {
       singleArray.push(s);
     }
   }
-
+  singleArray.sort((a, b) => a - b);
   console.log(singleArray);
 }
 
