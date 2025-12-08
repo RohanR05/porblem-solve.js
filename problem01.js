6f36111e2188ac9f9ac34b1b6ef4e4f539ee8d01
003-demo <rrriku05@gmail.com>
2025-12-08 19:36:45 +0600

    f

--- problem01.js
+++ problem01.js
@@ -1,4 +1,4 @@
-let mainArray = [1, 2, 31];
+let mainArray = [2, 2, 2, 2, 2];
 let ava = 0;
 let total = 0;
 
@@ -8,6 +8,10 @@ function AverageOfOddNum(str) {
       total = total + s;
       ava++;
     }
+  }
+  if (ava === 0) {
+    console.log("No Odd is here");
+    return null;
   }
   let average = total / ava;
   console.log(average);
