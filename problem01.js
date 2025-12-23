9bc6bfa5ab88f952109e45750477f6c91525374d
003-demo <rrriku05@gmail.com>
2025-12-23 20:00:09 +0600

    w

--- problem01.js
+++ problem01.js
@@ -2,5 +2,12 @@ document.getElementById("btn-submit").addEventListener("click", function () {
   const textArea = document.getElementById("textArea");
 
   const newComment = textArea.value;
-  console.log(newComment);
+
+  const mainContainer=document.getElementById("main-container")
+
+  const commentElement=document.createElement("p");
+  commentElement.classList.add('container')
+  commentElement.innerText=newComment
+
+  mainContainer.appendChild(commentElement)
 });
